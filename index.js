b41e377ff3d307762775dbcb0c8d32c71e8fc016
ChetanHimpact <101732423+ChetanHimpact@users.noreply.github.com>
2022-05-06 22:20:39 +0530

    substring without repeate

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const Express = require('express');
-const app = Express();
-const bodyParser = require('body-parser');
-require('dotenv').config();
+function lengthOfLongestSubstring(s) {
+  let stringLength = s.length;
+  let maxLength = 0;
+  const charMap = new Map();
+  let pos = 0;
 
-//Importing ROUTES
-const adminRoute = require('./routes/admin.route');
-const companyRoute = require('./routes/company.route');
-const employeeRoute = require('./routes/employee.route');
+  for (let i = 0; i < stringLength; i++) {
+    if (charMap.has(s[i])) {
+      pos = Math.max(charMap.get(s[i]), pos);
+    }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    maxLength = Math.max(maxLength, i - pos + 1);
+    charMap.set(s[i], i + 1);
+  }
+  return maxLength;
+}
 
-app.use('/api/v1/admin-service', adminRoute);
-app.use('/api/v1/company-service', companyRoute);
-app.use('/api/v1/employee-service', employeeRoute);
-
-app.listen(process.env.port, () => {
-  console.info(
-    `v${process.env.version} - listening on http://localhost:${process.env.port}`
-  );
-});
-
-module.exports = app;
+let inputString = 'abcda';
+console.log(lengthOfLongestSubstring(inputString));
