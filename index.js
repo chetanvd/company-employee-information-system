9f25c443e45de3d98c9d6bff60e86e3e93d0978b
ChetanHimpact <101732423+ChetanHimpact@users.noreply.github.com>
2022-05-07 11:11:00 +0530

    list all listids

--- index.js
+++ index.js
@@ -1,19 +1,445 @@
-function lengthOfLongestSubstring(s) {
-  let stringLength = s.length;
-  let maxLength = 0;
-  const charMap = new Map();
-  let pos = 0;
+let listIds = [];
+const input = {
+  _id: '5b98fa069f664f7e1ae7498c',
+  domains: [
+    {
+      type: 'domain',
+      list: [
+        {
+          listId: '5be177855e852b0e920ad136a',
+          weight: 10,
+        },
+        {
+          listId: '5be177a65e852b0e920ad137b',
+          weight: 10,
+        },
+        {
+          listId: '5be177eb5e852b0e920ad139c',
+          weight: 10,
+        },
+        {
+          listId: '5be1781b5e852b0e920ad13ad',
+          weight: 10,
+        },
+        {
+          listId: '5be1783d5e852b0e920ad13be',
+          weight: 10,
+        },
+      ],
+    },
+    {
+      type: 'domain',
+      children: [
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              children: [
+                {
+                  type: 'subdomain',
+                  list: [
+                    {
+                      listId: '5be177855e852b0e920ad136f',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be177a65e852b0e920ad137g',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be177eb5e852b0e920ad139h',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be1781b5e852b0e920ad13aj',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be1783d5e852b0e920ad13bk',
+                      weight: 10,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              children: [
+                {
+                  type: 'subdomain',
+                  list: [
+                    {
+                      listId: '5be177855e852b0e920ad136f',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be177a65e852b0e920ad137g',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be177eb5e852b0e920ad139h',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be1781b5e852b0e920ad13aj',
+                      weight: 10,
+                    },
+                    {
+                      listId: '5be1783d5e852b0e920ad13bk',
+                      weight: 10,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'domain',
+      children: [
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be177855e852b0e920ad136',
+                  weight: 10,
+                },
+                {
+                  listId: '5be177a65e852b0e920ad137',
+                  weight: 10,
+                },
+                {
+                  listId: '5be177eb5e852b0e920ad139',
+                  weight: 10,
+                },
+                {
+                  listId: '5be1781b5e852b0e920ad13a',
+                  weight: 10,
+                },
+                {
+                  listId: '5be1783d5e852b0e920ad13b',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be1785e5e852b0e920ad13c',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179015e852b0e920ad13d',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179255e852b0e920ad13e',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179495e852b0e920ad13f',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179d95e852b0e920ad140',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17a0c5e852b0e920ad141',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17a325e852b0e920ad142',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17b1f5e852b0e920ad143',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17b455e852b0e920ad144',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17b795e852b0e920ad145',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17ba35e852b0e920ad146',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17bc05e852b0e920ad147',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17c3c5e852b0e920ad148',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17c915e852b0e920ad149',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17cd45e852b0e920ad14a',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17d155e852b0e920ad14b',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17d575e852b0e920ad14c',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17db45e852b0e920ad14d',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'domain',
+      children: [
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be177855e852b0e920ad136',
+                  weight: 10,
+                },
+                {
+                  listId: '5be177a65e852b0e920ad137',
+                  weight: 10,
+                },
+                {
+                  listId: '5be177eb5e852b0e920ad139',
+                  weight: 10,
+                },
+                {
+                  listId: '5be1781b5e852b0e920ad13a',
+                  weight: 10,
+                },
+                {
+                  listId: '5be1783d5e852b0e920ad13b',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be1785e5e852b0e920ad13c',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179015e852b0e920ad13d',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179255e852b0e920ad13e',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179495e852b0e920ad13f',
+                  weight: 10,
+                },
+                {
+                  listId: '5be179d95e852b0e920ad140',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17a0c5e852b0e920ad141',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17a325e852b0e920ad142',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17b1f5e852b0e920ad143',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17b455e852b0e920ad144',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17b795e852b0e920ad145',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17ba35e852b0e920ad146',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17bc05e852b0e920ad147',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17c3c5e852b0e920ad148',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'subdomain',
+          children: [
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17c915e852b0e920ad149',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17cd45e852b0e920ad14a',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17d155e852b0e920ad14b',
+                  weight: 10,
+                },
+              ],
+            },
+            {
+              type: 'subdomain',
+              list: [
+                {
+                  listId: '5be17d575e852b0e920ad14c',
+                  weight: 10,
+                },
+                {
+                  listId: '5be17db45e852b0e920ad14d',
+                  weight: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-  for (let i = 0; i < stringLength; i++) {
-    if (charMap.has(s[i])) {
-      pos = Math.max(charMap.get(s[i]), pos);
+function subdomainLogger(index, subDomainArr, listIds) {
+  if (index < subDomainArr.length) {
+    if (subDomainArr[index].list) {
+      subDomainArr[index].list.forEach((element) => {
+        listIds.push(element.listId);
+      });
     }
 
-    maxLength = Math.max(maxLength, i - pos + 1);
-    charMap.set(s[i], i + 1);
+    if (subDomainArr[index].children) {
+      subdomainLogger(0, subDomainArr[index].children, listIds);
+    }
+
+    subdomainLogger(index + 1, subDomainArr, listIds);
   }
-  return maxLength;
 }
 
-let inputString = 'abcda';
-console.log(lengthOfLongestSubstring(inputString));
+var domainLogger = function (index, domainArr, listIds) {
+  if (index < domainArr.length) {
+    if (domainArr[index].list) {
+      domainArr[index].list.forEach((element) => {
+        listIds.push(element.listId);
+      });
+    }
+
+    if (domainArr[index].children) {
+      subdomainLogger(0, domainArr[index].children, listIds);
+    }
+
+    domainLogger(index + 1, domainArr, listIds);
+  }
+};
+
+domainLogger(0, input.domains, listIds);
+console.log(listIds, listIds.length);
